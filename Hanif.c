3024a9cf20ccb30bcbaf6ec7e7f22b38e9958613
Muhammad Hanif <muhammad.hanif.tif24@polban.ac.id>
2025-02-22 11:32:53 +0700

    membuat branch Haniif

---
+++ Hanif.c
@@ -0,0 +1,15 @@
+//
+// Created by Arman on 2/22/2025.
+//
+#include "raylib.h"
+#include "Hanif.h"
+
+#include <stdio.h>
+
+
+void game(){
+        BeginDrawing();
+        ClearBackground(DARKBLUE);
+        DrawText("Hello World!", 20, 20, 20, RAYWHITE);
+        EndDrawing();
+}
---
+++ Hanif.h
@@ -0,0 +1,10 @@
+//
+// Created by Arman on 2/22/2025.
+//
+
+#ifndef HANIF_H
+#define HANIF_H
+
+void game();
+
+#endif //HANIF_H
