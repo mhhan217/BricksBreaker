a07d580314d962115fa187405031c7ac5e0e02e3
ArnoldBilly <aarnoldbilly1907@gmail.com>
2025-02-24 10:57:02 +0700

    Perbaikan commit pertama

--- Include/Billy.h
+++ Include/Billy.h
@@ -16,8 +16,34 @@ typedef struct
 
 Balok bricks[BRICK_ROWS][BRICK_COLS];
 
-void inisialisasiBalok();
-void gambarBalok();
-void checkBenturanBalok();
+void inisialisasiBalok() {
+    int i=0;
+    while (i < BRICK_ROWS) {
+        int j=0;
+        while (j < BRICK_COLS){
+            bricks[i][j].Rectangle.x = j * (BRICK_WIDTH + BRICK_PADDING) + 38;
+            bricks[i][j].Rectangle.y = i * (BRICK_HEIGHT + BRICK_PADDING) + 38;
+            bricks[i][j].Rectangle.width = BRICK_WIDTH;
+            bricks[i][j].Rectangle.height = BRICK_HEIGHT;
+            bricks[i][j].on = true;
+            j++;
+        }
+        i++; 
+    }    
+}
+
+void gambarBalok() {
+    int i=0;
+    while (i < BRICK_ROWS) {
+        int j=0;
+        while (j < BRICK_COLS){
+            if (bricks[i][j].on) {
+                DrawRectangleRec(bricks[i][j].Rectangle, BLUE);
+            }
+            j++;
+        }
+        i++; 
+    }
+}
 
 #endif //BILLY_H
--- Modul/Billy.c
+++ Modul/Billy.c
@@ -25,36 +25,6 @@ int main() {
     return 0;
 }
 
-void inisialisasiBalok(){
-    int i=0;
-    while (i < BRICK_ROWS) {
-        int j=0;
-        while (j < BRICK_COLS){
-            bricks[i][j].Rectangle.x = j * (BRICK_WIDTH + BRICK_PADDING) + 38;
-            bricks[i][j].Rectangle.y = i * (BRICK_HEIGHT + BRICK_PADDING) + 38;
-            bricks[i][j].Rectangle.width = BRICK_WIDTH;
-            bricks[i][j].Rectangle.height = BRICK_HEIGHT;
-            bricks[i][j].on = true;
-            j++;
-        }
-        i++; 
-    }
-}
-
-void gambarBalok() {
-    int i=0;
-    while (i < BRICK_ROWS) {
-        int j=0;
-        while (j < BRICK_COLS){
-            if (bricks[i][j].on) {
-                DrawRectangleRec(bricks[i][j].Rectangle, BLUE);
-            }
-            j++;
-        }
-        i++; 
-    }
-}
-
 //void checkBenturanBalok() {
-
+//
 //}
