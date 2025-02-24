1d2e83e46c027dbc7bf503cdf6aebfe1ccb65561
chintakarinaa <chinta.karina.tif24@polban.ac.id>
2025-02-24 12:39:33 +0700

    Update Chinta.h

--- Include/Chinta.h
+++ Include/Chinta.h
@@ -1,5 +1,17 @@
 
 #ifndef CHINTA_H
 #define CHINTA_H
+#include "konfigurasi.h"
+
+void displayMenu();
+void playGame();
+void displayInfo();
+void displayLeaderboard();
+int loadScores();
+void saveScores();
+void displayGameOverMenu();
+int calculateFinalScore();
+void handleUserInput();
+void drawText(const char* text);
 
 #endif //CHINTA_H
