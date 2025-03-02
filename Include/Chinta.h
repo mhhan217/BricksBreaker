c2e58acf13b7572de4af54d53705280c833df568
chintakarinaa <chinta.karina.tif24@polban.ac.id>
2025-03-03 04:51:14 +0700

    Update Chinta.h

--- Include/Chinta.h
+++ Include/Chinta.h
@@ -1,10 +1,19 @@
+
 #ifndef CHINTA_H
 #define CHINTA_H
+#define BUTTON_WIDTH 200
+#define BUTTON_HEIGHT 40
+#define MY_DARK_PINK (Color){ 199, 21, 133, 255 }
+#define MY_BLUE (Color){ 0, 0, 255, 255 }
+#define MY_GREEN (Color){ 0, 255, 0, 255 }
+#define MY_YELLOW (Color){ 255, 255, 0, 255 }
+#define BLACK_BG (Color){ 0, 0, 0, 255 }
+#define WHITE_TEXT (Color){ 255, 255, 255, 255 }
+
 #include <stdio.h>
 #include "raylib.h"
 
-#define BUTTON_WIDTH 200
-#define BUTTON_HEIGHT 40
+extern int selectedDifficulty;
 
 bool DrawButton(Rectangle rect, const char *text, Color outlineColor, Color textColor);
 void displayMenuWithGraphics();
@@ -12,6 +21,5 @@ void displayDifficultyMenu();
 void displayInfo();
 void displaySettings();
 void playGame();
-int selectedDifficulty = 0;
 
 #endif //CHINTA_H
