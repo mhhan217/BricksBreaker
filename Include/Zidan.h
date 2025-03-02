880efe2e49105d8e5c7357bbb01813d68bdf52ab
Zidan <zidantaufiqurahman@gmail.com>
2025-03-02 13:17:54 +0700

    4

--- Include/Zidan.h
+++ Include/Zidan.h
@@ -3,13 +3,20 @@
 
 #include "Konfigurasi.h"
 
+#define TAMBAHAN_BARIS 2
+#define BATAS_WAKTU 10
+
 typedef struct {
     Rectangle rect;
     Color color;
     bool active;
 } Brick;
 
+
+
+
 void InisialisasiBricks(Brick bricks[JUMLAH_BARIS][JUMLAH_KOLOM]);
 void GambarBricks(Brick bricks[JUMLAH_BARIS][JUMLAH_KOLOM]);
+void TambahBarisBricks(Brick bricks[JUMLAH_BARIS + TAMBAHAN_BARIS][JUMLAH_KOLOM], int *barisSaatIni);
 
 #endif
