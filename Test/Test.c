6012f8861ef6855c36eb7f402ff153259399df0a
zahwanazalla <zahwanazalla@gmail.com>
2025-03-03 08:04:35 +0700

    perbaharui main

--- Test/Test.c
+++ Test/Test.c
@@ -1,35 +1,22 @@
 #include "zahwa.h"
 #include "konfigurasi.h"
 #include "raylib.h"
+#include "zahwa.c"
 
 int main() {
-    // Inisialisasi jendela permainan
-    InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "Bricks Breaker");
-
-    // Inisialisasi paddle dan bola
-    Paddle paddle;
     
-    InitPaddle(&paddle, (Vector2){ SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT - 30 }, (Vector2){ 100, 20 }, 5.0f);
+    InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "Bricks Breaker");
+    Paddle paddle;
+    InitPaddle(&paddle, (Vector2){ SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT - 50 }, (Vector2){ 100, 20 }, 5.0f);
+    SetTargetFPS(60); 
 
-    SetTargetFPS(60); // Batasi FPS ke 60
-
-    // Loop utama permainan
     while (!WindowShouldClose()) {
-        // Update paddle dan bola
         UpdatePaddle(&paddle);
-
-        // Mulai menggambar
-        BeginDrawing();
         ClearBackground(BLACK);
-
-        // Gambar paddle dan bola
         DrawPaddle(paddle);
-
         DrawText("Press ESC to exit", 10, 10, 20, WHITE);
-
         EndDrawing();
     }
 
-    CloseWindow(); // Menutup jendela permainan
-    return 0;
+    CloseWindow(); 
 }
