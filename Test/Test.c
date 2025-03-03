6fa295ae22b6665211526fe3a1b7ac4c8d3d9f8b
Muhammad Hanif <muhammad.hanif.tif24@polban.ac.id>
2025-03-03 08:08:20 +0700

    membuat arah bola lebih terkendali

--- Test/Test.c
+++ Test/Test.c
@@ -7,15 +7,20 @@ int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bricks Breaker");
     SetTargetFPS(TARGET_FPS);
 
+    Paddle paddle;
+    InitPaddle(&paddle, (Vector2){ SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT - 30 }, (Vector2){ 100, 20 }, 15.0f);
+
     // Inisialisasi bola
     Ball bola;
-    InitBall(&bola, (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2});
+    InitBall(&bola,(Vector2){0,-20},&paddle);
 
     while (!WindowShouldClose()) {
-        UpdateBall(&bola);
+        UpdateBall(&bola,&paddle,(Vector2){0,-20});
+        UpdatePaddle(&paddle);
         BeginDrawing();
         ClearBackground(BLACK);
         DrawBall(bola);
+        DrawPaddle(paddle);
         EndDrawing();
     }
     CloseWindow();
