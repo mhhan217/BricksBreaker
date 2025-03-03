8fce584c5f80209e7506f38dcf1bbff95b6b6d05
zahwanazalla <zahwanazalla@gmail.com>
2025-03-03 08:02:18 +0700

    tambah/kurang panjang paddle dan warna paddle random

--- Modul/Zahwa.c
+++ Modul/Zahwa.c
@@ -1,5 +1,5 @@
 #include "raylib.h"
-#include "Zahwa.h"
+#include "zahwa.h"
 
 void InitPaddle(Paddle* Paddle, Vector2 posisiAwal, Vector2 ukuran, float kecepatan) {
     Paddle->Posisi = posisiAwal;
@@ -15,9 +15,19 @@ void UpdatePaddle(Paddle* Paddle) {
     if (IsKeyDown(KEY_RIGHT) && Paddle->Posisi.x + Paddle->Ukuran.x < GetScreenWidth()) {
         Paddle->Posisi.x += Paddle->Kecepatan;
     }
+	// Jika tombol UP ditekan, perbesar paddle
+    if (IsKeyPressed(KEY_UP)) {
+        Paddle->Ukuran.x += 10;  // Tambah panjang paddle
+    }
+    // Jika tombol DOWN ditekan, perkecil paddle
+    if (IsKeyPressed(KEY_DOWN) && Paddle->Ukuran.x > 20) {
+        Paddle->Ukuran.x -= 10;  // Kurangi panjang paddle
+    }
+    if (IsKeyPressed(KEY_SPACE)) {
+        Paddle->Warna = (Color){ GetRandomValue(0, 255), GetRandomValue(0, 255), GetRandomValue(0, 255), 255 }; //ubah random warna paddle dengan space
+    }
 }
 
 void DrawPaddle(Paddle Paddle) {
     DrawRectangleV(Paddle.Posisi, Paddle.Ukuran, Paddle.Warna);
 }
-
