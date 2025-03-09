5df030a3027ae987a3ef8af8c92d5c8560b45f5e
faridhaaz11 <faridhazahiya11@gmail.com>
2025-03-09 18:56:20 +0700

    Exit

--- Modul/Faridha.c
+++ Modul/Faridha.c
@@ -1,20 +1,35 @@
 #include <string.h>  
 #include "Faridha.h"
+#include <stdlib.h>
 
 int gameState = 0;
 
 void InisialisasiLayarGameOver(LayarGameOver *layar) {
     strcpy(layar->pesan, "GAME OVER");  
-    layar->lebarLayar = LEBAR_LAYAR;
-    layar->tinggiLayar = TINGGI_LAYAR;
+    layar->lebarLayar = SCREEN_WIDTH;
+    layar->tinggiLayar = SCREEN_HEIGHT;
     layar->warnaTeks = RED;
+}
+
+void ResetGame() {
+    // Tambahkan logika reset game sesuai kebutuhan
+    gameState = 2; // Ganti dengan state permainan utama
+}
+
+void KembaliKeMenu() {
+    gameState = 3; // Ganti dengan state menu utama
+}
+
+void KeluarPermainan() {
+    CloseWindow();
+    exit(0);
 }
 
 void GambarLayarGameOver(LayarGameOver layar) {
     BeginDrawing();
     ClearBackground(BLACK);
 
-    Color warnaHuruf[] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, PINK, SKYBLUE};
+    Color warnaHuruf[] = {PINK, BLUE, GREEN, YELLOW};
     
     const char *judulGame = "Bricks Breaker Game";
     int posisiXJudulGame = layar.lebarLayar / 2 - MeasureText(judulGame, 30) / 2;
@@ -31,7 +46,7 @@ void GambarLayarGameOver(LayarGameOver layar) {
     int posisiYJudul = layar.tinggiLayar / 4;
     DrawText(judul, posisiXJudul, posisiYJudul, 40, RED);
 
-	int tombolX = layar.lebarLayar / 2 - 100;
+    int tombolX = layar.lebarLayar / 2 - 100;
     int tombolScoreY = layar.tinggiLayar / 2 - 60;
 
     DrawRectangleLines(tombolX, tombolScoreY, 200, 40, PINK);
@@ -41,15 +56,30 @@ void GambarLayarGameOver(LayarGameOver layar) {
         gameState = 1; // Beralih ke layar skor
     }
 
-    DrawRectangleLines(layar.lebarLayar / 2 - 100, layar.tinggiLayar / 2 - 10, 200, 40, BLUE);
+    int tombolRestartY = layar.tinggiLayar / 2 - 10;
+    DrawRectangleLines(tombolX, tombolRestartY, 200, 40, BLUE);
     DrawText("Restart", layar.lebarLayar / 2 - MeasureText("Restart", 20) / 2, layar.tinggiLayar / 2, 20, BLUE);
 
-    DrawRectangleLines(layar.lebarLayar / 2 - 100, layar.tinggiLayar / 2 + 40, 200, 40, GREEN);
+    if (CekKlikTombol(tombolX, tombolRestartY, 200, 40)) {
+        ResetGame();
+    }
+    
+    int tombolMenuY = layar.tinggiLayar / 2 + 40;
+    DrawRectangleLines(tombolX, tombolMenuY, 200, 40, GREEN);
     DrawText("Menu", layar.lebarLayar / 2 - MeasureText("Menu", 20) / 2, layar.tinggiLayar / 2 + 50, 20, GREEN);
 
-    DrawRectangleLines(layar.lebarLayar / 2 - 100, layar.tinggiLayar / 2 + 90, 200, 40, YELLOW);
+    if (CekKlikTombol(tombolX, tombolMenuY, 200, 40)) {
+        KembaliKeMenu();
+    }
+    
+    int tombolExitY = layar.tinggiLayar / 2 + 90;
+    DrawRectangleLines(tombolX, tombolExitY, 200, 40, YELLOW);
     DrawText("Exit", layar.lebarLayar / 2 - MeasureText("Exit", 20) / 2, layar.tinggiLayar / 2 + 100, 20, YELLOW);
 
+    if (CekKlikTombol(tombolX, tombolExitY, 200, 40)) {
+        KeluarPermainan();
+    }
+    
     EndDrawing();
 }
 
@@ -57,10 +87,10 @@ void GambarLayarScore() {
     BeginDrawing();
     ClearBackground(BLACK);
 
-    DrawText("SCORE", LEBAR_LAYAR / 2 - MeasureText("SCORE", 40) / 2, 100, 40, YELLOW);
-    DrawText("Skor Anda: 1000", LEBAR_LAYAR / 2 - MeasureText("Skor Anda: 1000", 30) / 2, 200, 30, WHITE);
+    DrawText("SCORE", SCREEN_WIDTH / 2 - MeasureText("SCORE", 40) / 2, 100, 40, YELLOW);
+    DrawText("Skor Anda: 1000", SCREEN_WIDTH / 2 - MeasureText("Skor Anda: 1000", 30) / 2, 200, 30, WHITE);
 
-    int tombolKembaliX = LEBAR_LAYAR / 2 - 100;
+    int tombolKembaliX = SCREEN_WIDTH / 2 - 100;
     int tombolKembaliY = 400;
 
     DrawRectangleLines(tombolKembaliX, tombolKembaliY, 200, 40, RED);
