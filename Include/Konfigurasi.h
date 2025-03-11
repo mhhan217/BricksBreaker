3399b59f415b6cdeb36d218a9c2edd8e623739fc
faridhaaz11 <faridhazahiya11@gmail.com>
2025-03-11 21:55:26 +0700

    level selection

--- Include/Konfigurasi.h
+++ Include/Konfigurasi.h
@@ -1,7 +1,27 @@
 #ifndef KONFIGURASI_H
 #define KONFIGURASI_H
 
-#define LEBAR_LAYAR 800
-#define TINGGI_LAYAR 600
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define TARGET_FPS 60
+
+typedef enum {
+    MENU,
+    HIGH_SCORE,
+    SETTINGS,
+    INFO,
+    PLAY,
+    PLAYING, 
+    PAUSE,
+    PAUSED, 
+    GAME_OVER,
+    EXIT,
+    LEVEL_SELECTION 
+} GameState;
+
+typedef struct {
+    GameState gameState; 
+    int index;   
+} ScreenControl;
 
 #endif
