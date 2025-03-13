13d503fdc31c7afa0e16f59e186139bc90309734
ArnoldBilly <aarnoldbilly1907@gmail.com>
2025-03-13 23:19:10 +0700

    Menambah pattern

--- Modul/Billy.c
+++ Modul/Billy.c
@@ -4,6 +4,23 @@
 #include "Hanif.h"
 #include <math.h>
 
+int currentLevel = 0;
+Sound suaratabrakan;
+
+void inisialisasibacksound() {
+    InitAudioDevice();
+    suaratabrakan = LoadSound("tabrakan.wav");
+}
+
+void panggilbacksound(){
+    PlaySound(suaratabrakan);
+}
+
+void tutupbacksound() {
+    UnloadSound(suaratabrakan);
+    CloseAudioDevice();
+}
+
 void inisialisasiBalok() {
     int i=0;
     while (i < BRICK_ROWS) {
@@ -13,7 +30,7 @@ void inisialisasiBalok() {
             bricks[i][j].kotak.y = i * (BRICK_HEIGHT + BRICK_PADDING) + 38;
             bricks[i][j].kotak.width = BRICK_WIDTH;
             bricks[i][j].kotak.height = BRICK_HEIGHT;
-            bricks[i][j].on = true;
+            bricks[i][j].on = levelPatterns[currentLevel][i][j] == 1;
             j++;
         }
         i++; 
@@ -49,6 +66,7 @@ void bolaterkenabalok(Ball* Ball) {
                 Ball->Posisi.y + Ball->Radius >= brick->kotak.y && 
                 Ball->Posisi.y <= brick->kotak.y + BRICK_HEIGHT) {
                     brick->on = false;
+                    panggilbacksound();
                     Ball->Kecepatan.y = -Ball->Kecepatan.y;
                     return;
             }
@@ -56,4 +74,22 @@ void bolaterkenabalok(Ball* Ball) {
         }
         i++;
     }
+}
+
+bool AreAllBricksDestroyed() {
+    for (int i = 0; i < BRICK_ROWS; i++) {
+        for (int j = 0; j < BRICK_COLS; j++) {
+            if (bricks[i][j].on) return false;
+        }
+    }
+    return true;
+}
+
+void NextLevel() {
+    if (currentLevel < LEVELS - 1) {
+        currentLevel++;
+    } else {
+        currentLevel = 0;
+    }
+    inisialisasiBalok();
 }
--- Test/Test.c
+++ Test/Test.c
@@ -4,22 +4,30 @@
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bricks Breaker");
     SetTargetFPS(TARGET_FPS);
+    inisialisasibacksound();
+
     Ball bola;
     Paddle paddle;
     InitPaddle(&paddle, (Vector2){ SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT - 30 }, (Vector2){ 100, 20 }, 15.0f);
     InitBall(&bola, (Vector2){SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2}, &paddle);
     inisialisasiBalok();
+
     while (!WindowShouldClose()) {
-        UpdateBall(&bola,&paddle,(Vector2){0,-20});
+        bolaterkenabalok(&bola);
+        UpdateBall(&bola, &paddle, (Vector2){0, -20});
         UpdatePaddle(&paddle);
+        if (AreAllBricksDestroyed()) {
+            NextLevel();
+        }
         BeginDrawing();
-        gambarBalok();
         ClearBackground(BLACK);
+        gambarBalok();
         DrawBall(bola);
         DrawPaddle(paddle);
         EndDrawing();
     }
+    
+    tutupbacksound();
     CloseWindow();
-
     return 0;
 }
