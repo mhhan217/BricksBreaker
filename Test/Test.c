628a6cafd789ada4844f575e711414846ecfc92b
zahwanazalla <zahwanazalla@gmail.com>
2025-03-17 02:42:38 +0700

    test main paddle

--- Test/Test.c
+++ Test/Test.c
@@ -1,29 +1,19 @@
 #include "raylib.h"
-#include "../Include/Hanif.h"
-#include "../Include/Konfigurasi.h"
+#include "../Include/Zahwa.h"
+#include "Konfigurasi.h"
 
 int main() {
-    // Inisialisasi window
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bricks Breaker");
-    SetTargetFPS(TARGET_FPS);
-
+    
+    InitWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "Bricks Breaker");
     Paddle paddle;
-    InitPaddle(&paddle, (Vector2){ SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT - 30 }, (Vector2){ 100, 20 }, 15.0f);
-
-    // Inisialisasi bola
-    Ball bola;
-    InitBall(&bola,(Vector2){0,-20},&paddle);
+    InitPaddle(&paddle, (Vector2){ SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT - 50 }, (Vector2){ 100, 20 }, 5.0f);
+    SetTargetFPS(60); 
 
     while (!WindowShouldClose()) {
-        UpdateBall(&bola,&paddle,(Vector2){0,-20});
         UpdatePaddle(&paddle);
-        BeginDrawing();
         ClearBackground(BLACK);
-        DrawBall(bola);
         DrawPaddle(paddle);
         EndDrawing();
     }
-    CloseWindow();
 
-    return 0;
 }
